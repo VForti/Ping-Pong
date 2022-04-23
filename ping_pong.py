77bed181c86e10c066f30f2042520618ffc7c544
VForti <99172698+VForti@users.noreply.github.com>
2022-04-23 10:41:08 +0300

    ping pong

--- ping_pong.py
+++ ping_pong.py
@@ -1,1 +1,23 @@
-print('Hello, world!')
+
+from pygame import*
+
+width, height = 600,500
+window = display.set_mode((width, height))
+
+backgraund_color = (200,255,255)
+window.fill(backgraund_color)
+display.set_caption('Ping Pong')
+
+
+
+game = True
+clock = time.Clock()
+FPS = 60
+
+while game:
+	for e in event.get():
+		if e.type == QUIT:
+			game = False
+
+	display.update()
+	clock.tick(FPS)
