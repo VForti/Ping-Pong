1fe2c9d7306f6f72ca48ea6dd28c0633cdbb9cbc
VForti <99172698+VForti@users.noreply.github.com>
2022-04-23 11:22:15 +0300

    Ping Pong

--- ping_pong.py
+++ ping_pong.py
@@ -1,23 +1,62 @@
+from pygame import *
 
-from pygame import*
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-width, height = 600,500
-window = display.set_mode((width, height))
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 120:
+            self.rect.y += self.speed
 
-backgraund_color = (200,255,255)
-window.fill(backgraund_color)
-display.set_caption('Ping Pong')
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 120:
+            self.rect.y += self.speed
 
 
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+display.set_caption('Ping pong')
 
 game = True
+finish = False
 clock = time.Clock()
 FPS = 60
 
-while game:
-	for e in event.get():
-		if e.type == QUIT:
-			game = False
+racket1 = Player('racket1.png', 30, 200, 4, 12, 120)
+racket2 = Player('racket2.png', 550, 200, 4, 12, 120)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
 
-	display.update()
-	clock.tick(FPS)
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if not finish:
+        window.fill(back)
+
+        racket1.update_l()
+        racket2.update_r()
+
+        racket1.reset()
+        racket2.reset()
+
+    display.update()
+    clock.tick(FPS)
